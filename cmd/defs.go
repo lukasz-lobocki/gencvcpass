1afbf15256a52c576c68f72849242bc57e34efdf
Lukasz Lobocki <lukasz.lobocki@googlemail.com>
2026-02-28 15:43:40 +0100

    feat: Add option to exclude non-polish consonants

--- cmd/defs.go
+++ cmd/defs.go
@@ -3,16 +3,18 @@ package cmd
 const (
 	MAX_LOGGING_LEVEL int = 3 // Maximum allowed logging level.
 	CONSONANTS            = "bcdfghjklmnpqrstvwxyz"
+	CONSONANTS_PL         = "bcdfghjklmnprstwz"
 	VOWELS                = "aeiu"
-	DIGITS                = "23456789"
+	DIGITS                = "023456789"
 	CHUNKSIZE             = 6
 )
 
 type tConfig struct {
-	setsNum   int
-	digitsNum int
-	upperNum  int
-	separator string
+	setsNum       int
+	digitsNum     int
+	upperNum      int
+	separator     string
+	lessNonPolish bool
 }
 
 // operation implements function calculating positions of UPPER and digits
